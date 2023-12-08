52eae2fe3c97764cb80a1c23a93f01d074b6b0ea
Marcos Doff <marcos.doff@gmail.com>
2023-12-08 19:45:08 -0300

    Adds GLFW and GLEW
    Signed-off-by: Marcos Doff <marcos.doff@gmail.com>

--- RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing.cpp
@@ -8,8 +8,19 @@
 #include "color.h"
 #include "material.h"
 
+#include <GLFW/glfw3.h>
+
 int main()
 {
+	GLFWwindow* window;
+
+	if(!glfwInit())
+	{
+		return -1;
+	}
+
+	window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+
 	//world
 
 	HittableList world;
